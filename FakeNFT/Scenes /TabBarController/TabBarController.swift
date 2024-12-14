0a0574430d3ed1747e9cfdeedd8753ef0b391e6d
Ilya Lotnik <i@lotnik.ru>
2024-12-15 00:14:55 +0300

    Настроить UITabBarController для всех экранов, добавить иконки экранов

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -4,22 +4,61 @@ final class TabBarController: UITabBarController {
 
     var servicesAssembly: ServicesAssembly!
 
+    private let profileTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.profile", comment: ""),
+        image: UIImage(named: "ProfileTabBarItemImage"),
+        tag: 0
+    )
+
     private let catalogTabBarItem = UITabBarItem(
         title: NSLocalizedString("Tab.catalog", comment: ""),
-        image: UIImage(systemName: "square.stack.3d.up.fill"),
+        image: UIImage(named: "CatalogTabBarItemImage"),
+        tag: 0
+    )
+
+    private let cartTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.cart", comment: ""),
+        image: UIImage(named: "CartTabBarItemImage"),
+        tag: 0
+    )
+
+    private let statisticsTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.statistics", comment: ""),
+        image: UIImage(named: "StatisticsTabBarItemImage"),
         tag: 0
     )
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let profileController = TestProfileViewController(
+            servicesAssembly: servicesAssembly
+        )
+
         let catalogController = TestCatalogViewController(
             servicesAssembly: servicesAssembly
         )
+
+        let cartController = TestCartViewController(
+            servicesAssembly: servicesAssembly
+        )
+
+        let statisticsController = TestStatisticsViewController(
+            servicesAssembly: servicesAssembly
+        )
+
+        profileController.tabBarItem = profileTabBarItem
         catalogController.tabBarItem = catalogTabBarItem
+        cartController.tabBarItem = cartTabBarItem
+        statisticsController.tabBarItem = statisticsTabBarItem
 
-        viewControllers = [catalogController]
+        viewControllers = [profileController, catalogController, cartController, statisticsController]
 
-        view.backgroundColor = .systemBackground
+        setupUI()
+    }
+
+    private func setupUI() {
+        tabBar.unselectedItemTintColor = UIColor(named: "ypBlack")
+        tabBar.backgroundColor = UIColor(named: "ypWhite")
     }
 }
