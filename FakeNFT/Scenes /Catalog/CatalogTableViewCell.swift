75fbffcea26d5b7563d9b7d271524f85cb0294ab
Ilya Lotnik <i@lotnik.ru>
2024-12-24 20:22:40 +0300

    Каталог 1 - Сверстать ячейку table(collection?) view #43

---
+++ FakeNFT/Scenes /Catalog/CatalogTableViewCell.swift
@@ -0,0 +1,81 @@
+//
+//  CatalogTableViewCell.swift
+//  FakeNFT
+//
+//  Created by Ilya Lotnik on 23.12.2024.
+//
+
+import UIKit
+
+
+final class CatalogTableViewCell: UITableViewCell {
+    static let reuseIdentifier = "CatalogTableViewCell"
+    weak var delegate: CatalogViewControllerProtocol?
+
+    private let cellTitleImage: UIImageView = {
+        let view = UIImageView()
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 12
+        view.contentMode = .scaleAspectFill
+        return view
+    }()
+
+    private let cellTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 17, weight: .bold)
+        label.textColor = Asset.ypBlack.color
+        return label
+    }()
+
+    private let cellStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 4
+        stackView.distribution = .fillProportionally
+        return stackView
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
+    public override func layoutSubviews() {
+        super.layoutSubviews()
+
+        contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 4, left: 0, bottom: 4, right: 0))
+    }
+
+    public override func prepareForReuse() {
+        super.prepareForReuse( )
+    }
+
+    func updateCell(titleLabel: String, titleImage: UIImage) {
+        DispatchQueue.main.async {
+            self.cellTitleLabel.text = titleLabel
+            self.cellTitleImage.image = titleImage
+        }
+    }
+
+    private func setupUI() {
+
+        [cellTitleImage, cellTitleLabel].forEach {
+            cellStackView.addArrangedSubview($0)
+        }
+
+        cellStackView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.addSubview(cellStackView)
+
+        NSLayoutConstraint.activate( [
+            cellStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+            cellStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+            cellStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -13),
+            cellStackView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            cellStackView.heightAnchor.constraint(equalToConstant: 179),
+        ])
+    }
+}
